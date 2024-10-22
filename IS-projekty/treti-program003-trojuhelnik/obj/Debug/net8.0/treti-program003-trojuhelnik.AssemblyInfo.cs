de73ebe8d49c6ff7c0c0124ebd3a05350ccf5508
Liliana Skramuská <liliana.skramuska@tul.cz>
2024-10-22 15:51:42 +0200

    program003

--- IS-projekty/treti-program003-trojuhelnik/obj/Debug/net8.0/treti-program003-trojuhelnik.AssemblyInfo.cs
+++ IS-projekty/treti-program003-trojuhelnik/obj/Debug/net8.0/treti-program003-trojuhelnik.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("treti-program003-trojuhelnik")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b6b9199529b25a9e63231e405d929d488e37791")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2483c42274dbedad03b7bcf0d7b102ce945a836d")]
 [assembly: System.Reflection.AssemblyProductAttribute("treti-program003-trojuhelnik")]
 [assembly: System.Reflection.AssemblyTitleAttribute("treti-program003-trojuhelnik")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
