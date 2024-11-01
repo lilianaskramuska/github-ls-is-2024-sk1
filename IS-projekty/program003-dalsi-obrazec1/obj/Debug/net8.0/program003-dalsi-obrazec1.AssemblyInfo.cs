c7725194983f8b6ff722a59c100f7158eae8824f
Liliana Skramuská <liliana.skramuska@tul.cz>
2024-11-01 14:32:38 +0100

    #4 program obrazec1

--- IS-projekty/program003-dalsi-obrazec1/obj/Debug/net8.0/program003-dalsi-obrazec1.AssemblyInfo.cs
+++ IS-projekty/program003-dalsi-obrazec1/obj/Debug/net8.0/program003-dalsi-obrazec1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program003-dalsi-obrazec1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+458d595ad5c00363cff7b2a9b4eb7121ef9d5d3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f587e98ffa8288c5562d5b52a9ea9b341e303c08")]
 [assembly: System.Reflection.AssemblyProductAttribute("program003-dalsi-obrazec1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program003-dalsi-obrazec1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
