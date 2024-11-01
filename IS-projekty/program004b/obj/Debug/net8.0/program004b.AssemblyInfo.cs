89b0cbff5cc62202101916f35f08e9bfb768b073
Liliana Skramuská <liliana.skramuska@tul.cz>
2024-11-01 13:42:52 +0100

    #6 program generator cisel

--- IS-projekty/program004b/obj/Debug/net8.0/program004b.AssemblyInfo.cs
+++ IS-projekty/program004b/obj/Debug/net8.0/program004b.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program004b")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3abbff28c73714f58a7d64a345dbd6e4ce66221")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4402b5d51c504e28c744b275d68340430de00ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("program004b")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program004b")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
