1b916d88c54793282a7323ec9c21361710a52e1f
Liliana Skramuská <liliana.skramuska@tul.cz>
2024-11-27 06:47:26 +0100

    #13 program

---
+++ IS-projekty/program012-intervaly/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Security.Cryptography;
+
+string again = "a";
+
+        while (again == "a") {
+            Console.Clear();
+            Console.WriteLine("*****************************************");
+            Console.WriteLine("**** ****");
+            Console.WriteLine("*****************************************");
+            Console.WriteLine("********* Liliana Skramuska *************");
+            Console.WriteLine("*****************************************");
+
+Console.WriteLine("Zadejte počet generovaných čísel (celé číslo): ");
+int n;
+while(!int.TryParse(Console.ReadLine(),out n))
+
+// hranata zavorka jednorozmerne pole - deklarace pole
+int[] myArray = new int[n];
+
+int kladna = 0;
+int zaporna = 0;
+int nuly = 0;
+
+Random randomNumber = new Random();
+
+// odrádkování
+Console.WriteLine("/n/nNáhodná čísla: ");
+for(int i=0; i<n; i ++ );{
+    myArray[i] = randomNumber.Next(dm, hm);
+    Console.Write("{0}; ", myArray[i]);
+
+for(myArray[i]<= (0.25 * hm)) {
+    int1++;
+    
+    {
+    else if(myArray[i] <= (0.5 * hm))
+    int2++
+    }
+
+    {
+    else if(myArray[i] <= (0.75 * hm))
+    int3++
+    }
+        else
+        int4++
+}
+
+ Console.WriteLine("\n Interval < ");
+  Console.WriteLine();
+            Console.WriteLine("Pro opakování programu stiskněte klávesu 'a'. Pro ukončení stiskněte jinou klávesu.");
+            again = Console.ReadLine();
